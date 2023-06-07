d9da41ffa41c639b33044ad9c11b405094d3d2c9
Stefano Cislaghi <stefano.cislaghi@aexp.com>
2023-06-07 19:48:42 +0100

    feat(parser): code improvements

---
+++ src/helpers.ts
@@ -0,0 +1,13 @@
+export const ROOT_TYPE_LOCALE = {
+	DIRECTIVE: { singular: "directive", plural: "directives" },
+	ENUM: { singular: "enum", plural: "enums" },
+	INPUT: { singular: "input", plural: "inputs" },
+	INTERFACE: { singular: "interface", plural: "interfaces" },
+	MUTATION: { singular: "mutation", plural: "mutations" },
+	OPERATION: { singular: "operation", plural: "operations" },
+	QUERY: { singular: "query", plural: "queries" },
+	SCALAR: { singular: "scalar", plural: "scalars" },
+	SUBSCRIPTION: { singular: "subscription", plural: "subscriptions" },
+	OBJECT: { singular: "object", plural: "objects" },
+	UNION: { singular: "union", plural: "unions" },
+};
